8f2c61b3adb82fea528a189ed8cf7ca6b32cd810
adarsh <adarsh.soni@tothenew.com>
2020-06-11 15:52:23 +0530

    erp react

---
+++ src/containers/Customer/ProductVariation/Design/Design.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import img1 from '../../../../assets/images/er1.png'
+import img2 from '../../../../assets/images/er.jpg'
+import img3 from '../../../../assets/images/er3.png'
+import classes from './Design.module.css';
+
+
+const proprietes = {
+    duration: 5000,
+    transitionDuration: 500,
+    infinite: true,
+    indicators: true,
+    arrows: true
+}
+
+const Slideshow = () => {
+    return (
+    <div className={classes.containerSlide}>
+        <Slide {...proprietes}>
+            <div className="each-slide">
+                <div>
+                    <img src={img1} alt="img1" />
+                </div>
+            </div>
+            <div className="each-slide">
+                <div>
+                    <img src={img2} alt="img2" />
+                </div>
+            </div>
+            <div className="each-slide">
+                <div>
+                    <img src={img3} alt="img3" />
+                </div>
+            </div>
+        </Slide>
+    </div>
+    )
+}
+
+export default Slideshow
